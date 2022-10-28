457cbfd77121c137cd0b50ea0103e3690afb0cbc
ubyquit <ingsystem.out.print@gmail.com>
2022-10-28 14:35:04 -0500

    inicio

---
+++ ejercicio5/assets/js/script.js
@@ -0,0 +1,4 @@
+let a = prompt("Ingresa un numero") ;
+let b = prompt("Ingresa otro numero");
+
+document.write("El numero es: " + (a*b));
---
+++ ejercicio6/assets/js/script.js
@@ -0,0 +1,15 @@
+//Nunca var
+let a = 10;
+let b = 10.5;
+let c = "Hola";
+let d = "h";
+let e = true;
+let f = false;
+
+
+document.write(typeof a + "<br>");
+document.write(typeof b + "<br>");
+
+document.write(`El tipo de dato es ${typeof a} <br>`);
+
+
