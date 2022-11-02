812e5f2be4bb796be4723e7303a118c22b130c53
ubyquit <ingsystem.out.print@gmail.com>
2022-11-02 12:45:47 -0500

    operadores de asignación

---
+++ ejercicio7/assets/js/script.js
@@ -0,0 +1,9 @@
+let a = 2;
+let b = parseInt(prompt("Ingresa el valor de b"));
+
+document.write(`${a} + ${b} = ${a + b} <br>`);
+document.write(`${a} - ${b} = ${a - b} <br>`);
+document.write(`${a} * ${b} = ${a * b} <br>`);
+document.write(`${a} / ${b} = ${a / b} <br>`);
+document.write(`${a} ** ${b} = ${a ** b} <br>`);
+document.write(Math.sqrt(a)); // Raiz cuadrada
---
+++ ejercicio8/assets/js/script.js
@@ -0,0 +1,10 @@
+let a = 0;
+let b = 10;
+
+document.write(`Asignación a:${a} = b:${b} es: ${(a = b)} <br>`);
+document.write(`Asignación de adición a:${a} += b:${b} es: ${(a += b)} <br>`);
+document.write(`Asignación de resta a:${a} -= b:${b} es: ${(a -= b)} <br>`);
+document.write(`Asignación de multi a:${a} *= b:${b} es: ${(a *= b)} <br>`);
+document.write(`Asignación de divi a:${a} /= b:${b} es: ${(a /= b)} <br>`);
+document.write(`Asignación de residuo a:${a} %= b:${b} es: ${(a %= b)} <br>`);
+document.write(`Asignación de potencia a:${a} **= b:${b} es: ${(a **= b)} <br>`);
