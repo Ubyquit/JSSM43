129c192f53946eb9cd23d35a159f7661b0aee544
ubyquit <ingsystem.out.print@gmail.com>
2022-11-10 08:44:14 -0500

    Funciones Flecha

---
+++ ejercicio16/assets/js/script.js
@@ -0,0 +1,14 @@
+/*
+ * FUNCIONES FLECHA
+ */
+
+const suma = (a, b) => {
+  sumas = a + b;
+  document.write(sumas + "<br>");
+};
+
+suma(5, 6);
+suma(7, 6);
+
+const nombres = nombre => document.write(nombre);
+nombres("Luis");
