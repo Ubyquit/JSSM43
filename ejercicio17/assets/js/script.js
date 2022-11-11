494cf91f82d866c560e490078cf77f507fc707c7
ubyquit <ingsystem.out.print@gmail.com>
2022-11-11 13:41:26 -0500

    FOR

---
+++ ejercicio17/assets/js/script.js
@@ -0,0 +1,17 @@
+/*
+ * INCREMENT = i o j
+ * DECREMENT = d
+ * COUNT = c
+ */
+
+for (let i = 0; i <= 10; i++) {
+  document.write(`El valor de i es: ${i} <br>`);
+}
+
+for (let d = 10; d >= 0; d--) {
+  document.write(`El valor de d es: ${d} <br>`);
+}
+
+let c;
+for (c = 0; c <= 10; c++);
+document.write(`El valor de c es: ${c} <br>`);
